e40fda8a930d6300545bdbb7ab894e2c9779ff24
John Sundell <john@sundell.co>
2017-03-13 19:46:47 +0100

    Add: AddSuffix
    This script will add a given suffix to all files in the current folder

---
+++ AddSuffix/README.md
@@ -0,0 +1,10 @@
+# AddSuffix
+
+Add a given suffix to the names of all files in the folder the script is run in.
+
+**Example**
+
+Add '@2x' to all files (for example images) in a folder:
+```
+$ marathon run addSuffix "@2x"
+```
---
+++ AddSuffix/addSuffix.swift
@@ -0,0 +1,19 @@
+import Foundation
+import Files
+
+let arguments = ProcessInfo.processInfo.arguments
+
+guard arguments.count > 1 else {
+    print("👮  No suffix given")
+    exit(1)
+}
+
+let suffix = arguments[1]
+
+for file in FileSystem().currentFolder.files {
+    let previousName = file.name
+    try file.rename(to: file.nameExcludingExtension + suffix)
+    print("👉  Renamed \(previousName) to \(file.name)")
+}
+
+print("✅  All done!")
